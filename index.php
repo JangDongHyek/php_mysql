d6154354a97a9acbd13c2486e01ba0b247425ada
JangDongHyek <JangDongHyek@gmail.com>
2019-01-30 00:20:18 +0900

    PHP MYSQL START

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
+  <body>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    html is...
+  </body>
+</html>
